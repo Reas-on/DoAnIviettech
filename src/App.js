b487f668f102bf50895ae1243bb92f63b6cbd048
ChuChu Zihu Ms <mjssdn95@gmail.com>
2024-05-07 20:06:17 +0700

    tuan fe admin

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ShopCategory from './Pages/ShopCategory';
-import Product from './Pages/Product';
-import Cart from './Pages/Cart';
-import LoginSignup from './Pages/LoginSignup';
-import Footer from './Components/Footer/Footer';
-import Shop from './Pages/Shop';
-import Profile from './Components/Profile/Profile';
-import men_banner from './Components/Assets/banner_mens.png';
-import women_banner from './Components/Assets/banner_women.png';
-import kid_banner from './Components/Assets/banner_kids.png';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Shop from "./Pages/Shop";
+import Profile from "./Components/Profile/Profile";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
+import MainLayout from "./layouts/MainLayout";
+import AdminLayout from "./layouts/AdminLayout";
+import DashBoard from "./Components/DashBoard/DashBoard";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Navbar />
         <Routes>
-          <Route path='/' element={<Shop />} />
-          <Route path='/mens' element={<ShopCategory banner={men_banner} category="men" />} />
-          <Route path='/womans' element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
-          <Route path='/product/:productId' element={<Product />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/login' element={<LoginSignup />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Shop />} />
+            <Route
+              path="/mens"
+              element={<ShopCategory banner={men_banner} category="men" />}
+            />
+            <Route
+              path="/womans"
+              element={<ShopCategory banner={women_banner} category="women" />}
+            />
+            <Route
+              path="/kids"
+              element={<ShopCategory banner={kid_banner} category="kid" />}
+            />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/login" element={<LoginSignup />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route index element={<DashBoard/>} />
+          </Route>
         </Routes>
-        <Footer />
       </BrowserRouter>
     </div>
   );
---
+++ src/Components/AddProduct/AddProduct.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import "./AddProduct.css";
+import upload_area from "../../assets/upload_area.svg";
+import { useState } from "react";
+import { Await } from "react-router-dom";
+const AddProduct = () => {
+  const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    old_price: "",
+    new_price: "",
+    category: "women",
+    image: "",
+  });
+  const imageHandler = (e) => {
+    setImage(e.target.files[0]);
+  };
+  const changeHandler = (e) => {
+    setProductDetails({
+      ...productDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+  // Add product function
+  const Add_Product = async () => {
+    try {
+      console.log(productDetails);
+      let responseData;
+      let product = productDetails;
+
+      let formData = new FormData();
+      formData.append("product", image);
+
+      const uploadResponse = await fetch("http://localhost:4000/upload", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+        body: formData,
+      });
+
+      responseData = await uploadResponse.json();
+
+      if (responseData.success) {
+        product.image = responseData.image_url;
+
+        const addProductResponse = await fetch(
+          "http://localhost:4000/addproduct",
+          {
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(product),
+          }
+        );
+
+        responseData = await addProductResponse.json();
+
+        if (responseData.success) {
+          alert(
+            `Product Added Successfully:\nName: ${product.name}\nNew Price: ${product.new_price}\nOld Price: ${product.old_price}\nCategory: ${product.category}\nImage: ${product.image}`
+          );
+        } else {
+          alert("Product Not Added");
+        }
+      }
+    } catch (error) {
+      console.error("Error adding product:", error.message);
+      alert("Error adding product: " + error.message);
+    }
+  };
+
+  return (
+    <div className="addproduct">
+      <div className="addproduct-itemfield">
+        <p>Product title</p>
+        <input
+          value={productDetails.name}
+          onChange={changeHandler}
+          type="text"
+          name="name"
+          placeholder="Enter product name"
+        />
+      </div>
+      <div className="addproduct-price">
+        <p>Price</p>
+        <input
+          value={productDetails.old_price}
+          onChange={changeHandler}
+          type="text"
+          name="old_price"
+          placeholder="Enter product price"
+        />
+      </div>
+      <div className="addproduct-price">
+        <p>Offer Price</p>
+        <input
+          value={productDetails.new_price}
+          onChange={changeHandler}
+          type="text"
+          name="new_price"
+          placeholder="Enter product price"
+        />
+      </div>
+      <div className="addproduct-itemfield">
+        <p>Product Category</p>
+        <select
+          value={productDetails.category}
+          onChange={changeHandler}
+          name="category"
+          className="add-product-selector"
+        >
+          <option value="women">women</option>
+          <option value="men">men</option>
+          <option value="kid">kid</option>
+        </select>
+      </div>
+      <div className="addproduct-itemfield">
+        <label htmlFor="file-input">
+          <img src={image ? URL.createObjectURL(image) : upload_area} alt="" />
+          <p>Upload product image</p>
+        </label>
+        <input
+          onChange={imageHandler}
+          type="file"
+          name="image"
+          id="file-input"
+          hidden
+        />
+      </div>
+      <button
+        onClick={() => {
+          Add_Product();
+        }}
+        className="addproduct-btn"
+      >
+        Add Product
+      </button>
+    </div>
+  );
+};
+
+export default AddProduct;
